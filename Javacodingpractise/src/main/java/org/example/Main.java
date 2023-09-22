2874d7df6e822b85c87d1aa7ee5b30dcd4192e92
gusudhakar <sudhakarguguloth1999@gmail.com>
2023-09-23 01:39:11 +0530

    my second commit

--- Javacodingpractise/src/main/java/org/example/Main.java
+++ Javacodingpractise/src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("my first code");
         System.out.println("my second step");
+        System.out.println("one more try");
     }
 }
